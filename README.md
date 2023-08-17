6ad7a10d5aa5b07b2a21c355b4bf7311e081ab82
Atsawa Chakhonmettkul <atsawa@calcomp.co.th>
2023-08-17 13:40:44 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Automation_with_python
 This repo is a list of automation idea Built primarily with Python.
+
+## am_py_1.py 
--- am_py_1.py
+++ am_py_1.py
@@ -1,3 +1,14 @@
 import requests # http requests
 
 from bs4 import BeautifulSoup # web scraping
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import datetime
+
+now = datetime.datetime.now()
+
+content = ''
+
+
